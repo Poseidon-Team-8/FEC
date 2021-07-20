9cbb56db9f9367191e1e2fe7e82981fa18daf413
Bruno <bberastain@gmail.com>
2021-07-20 16:24:18 -0700

    created file structure, installed dependencies, scripts to transpile and run server

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # FEC
-Front End Capstone project. 
+Front End Capstone project.
+
+run yarn
+- npm install -g yarn
+- yarn set version berry
+- yarn init
+(install dependencies from package.json)
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/src');
+var DIST_DIR = path.join(__dirname, '/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    contentBase: DIST_DIR,
+    compress: true,
+    port: 8080
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react"
+            ]
+          }
+        }
+      }
+    ]
+  }
+};
