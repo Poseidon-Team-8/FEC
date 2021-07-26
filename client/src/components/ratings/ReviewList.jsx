691100214256f95eea56cb6a7936279a0324ddf5
Sho Kondo <notshokondo@gmail.com>
2021-07-26 15:43:37 -0700

    added functionality for partial star rating

--- client/src/components/ratings/ReviewList.jsx
+++ client/src/components/ratings/ReviewList.jsx
@@ -8,7 +8,6 @@ let ReviewList = ({ reviews }) => {
           return <ReviewTile review={ review }/>
         })}
     </div>
-
   )
 }
 
--- client/src/components/ratings/StarRating.jsx
+++ client/src/components/ratings/StarRating.jsx
@@ -9,12 +9,21 @@ let StarRating = ({ rating }) => {
   for (let i = 0; i < numFull; i++) {
     stars.push(<img src='./icons/star.svg'></img>)
   }
+  if (numFraction > 0) {
+    let fraction = (numFraction > 0 && numFraction <= 0.25) ? './icons/star-one-quarter.svg' :
+                   (numFraction > 0.25 && numFraction <= 0.5) ? './icons/star-half.svg' :
+                   (numFraction > 0.5 && numFraction <= 0.75) ? './icons/star-three-quarter.svg' :
+                    './icons/star.svg'
 
-  if (!numFraction) {
+    stars.push(<img src={fraction}></img>)
+  }
+
+  if (numEmpty > 0) {
     for (let i = 0; i < numEmpty; i++) {
       stars.push(<img src='./icons/no-star.svg'></img>)
     }
   }
+
   return (
     stars
   );
