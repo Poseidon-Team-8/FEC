cb8405ecce0100dcc89d21e2ed5b9bcba3d42cb4
Sho Kondo <notshokondo@gmail.com>
2021-07-28 14:14:23 -0700

    code removal

--- client/src/components/ratings/PhotoModal.jsx
+++ client/src/components/ratings/PhotoModal.jsx
@@ -1,26 +1,4 @@
 import React, { useState, useContext } from 'react';
-import styled from 'styled-components';
-
-const Background = styled.div`
-  min-width: 100vw;
-  min-height: 100vh;
-  background: rgba(0, 0, 0, 0.6);
-  position: fixed;
-  justify-content: center;
-  align-items: center;
-`
-const ModalWrapper = styled.div`
-  width: 100%;
-  height: 100%;
-  background: #000;
-  z-index: 10;
-`
-
-const ModalImg = styled.img`
-  width: 100%;
-  height: 100%;
-  background: #000;
-`
 
 const PhotoModal = ({ displayModal, setDisplayModal, imgSrc }) => {
   return (
