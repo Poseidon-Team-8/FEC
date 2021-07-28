850ba2447d5a8ed14c529eaab5f9fba45f4502e4
Sho Kondo <notshokondo@gmail.com>
2021-07-27 18:56:17 -0700

    created conditional rendering for review response

--- client/src/components/ratings/ReviewTile.jsx
+++ client/src/components/ratings/ReviewTile.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from 'react';
 import StarRating from './StarRating.jsx';
 import Helpful from './Helpful.jsx';
+import Response from './Response.jsx'
 
 const ReviewTile = ({ review }) => {
   const { review_id, rating, summary, recommend, response, body, date, reviewer_name, helpfulness, photos } = review;
@@ -24,6 +25,7 @@ const ReviewTile = ({ review }) => {
         <b>{ summary }</b>
         <p>{ body }</p>
         { recommend ? <p>I recommend this product</p> : null }
+        { response ? <Response response={ response } /> : null}
         <Helpful yesCount={ helpfulness } reviewId={ review_id }/>
       </div>
     </div>
