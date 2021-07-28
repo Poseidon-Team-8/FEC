e15911999fb909f8950a77760687dce0dcc2ac2e
Sho Kondo <notshokondo@gmail.com>
2021-07-28 13:38:26 -0700

    added link to show more text if body is longer than 250 char

--- client/src/components/ratings/ReviewBody.jsx
+++ client/src/components/ratings/ReviewBody.jsx
@@ -2,22 +2,31 @@ import React, { useState, useContext } from 'react';
 
 
 const ReviewBody = ({ body }) => {
-  const [preview, setPreview] = useState(true);
-  const [fullText, setFullText] = useState(false);
+  const [showPreview, setShowPreview] = useState(true);
   const altView = (body.length > 250);
-  return (
-    <>
-    {
-      altView ?
-      (
-        null
-      ) :
-      (
-        <p>{ body }</p>
+
+  if (altView) {
+    const preview = body.slice(0, 250);
+    if (showPreview) {
+      return (
+        <>
+          <p>{ preview }</p>
+          <a className="extend-text" onClick={ () => setShowPreview(false)}>Show more</a>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <p>{ body }</p>
+          <a className="extend-text" onClick={ () => setShowPreview(true)}>Show less</a>
+        </>
       )
     }
-    </>
-  );
+  } else {
+    return (
+      <p>{ body }</p>
+    );
+  }
 }
 
 export default ReviewBody;
