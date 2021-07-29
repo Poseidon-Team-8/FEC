8d363b8919d161928b7be1a7f53e6845fa2581db
Sho Kondo <notshokondo@gmail.com>
2021-07-29 12:08:37 -0700

    set up product breakdown

--- client/src/components/ratings/CharacteristicEntry.jsx
+++ client/src/components/ratings/CharacteristicEntry.jsx
@@ -1,9 +1,30 @@
 import React, { useState, useContext } from 'react';
+import styled from 'styled-components';
 
-const CharacteristicEntry = () => {
+const CharacteristicEntry = ({ characteristic, value }) => {
+  console.log('Characteristic:', characteristic);
+  console.log('Value: ', value)
+
+  const Track = styled.div`
+    width: 60%;
+    height: 10px;
+    background-color: #222;
+    border-radius: 5px;
+  `;
+
+  const Thumb = styled.div`
+    width: ${props => props.percentage}%;
+    height: 100%;
+    background-color: #17b978;
+    border-radius: 2px;
+  `;
+
   return (
     <div className="characteristic-entry">
-
+      <p>{ characteristic }</p>
+      <Track>
+        <Thumb />
+      </Track>
     </div>
   );
 }
