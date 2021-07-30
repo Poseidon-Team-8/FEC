1bbc81744758eac9045cbfa64bb0e0c4c1b23a0e
Sho Kondo <notshokondo@gmail.com>
2021-07-30 14:30:40 -0700

    default review list is sorted by relevance

--- client/src/components/ratings/ReviewList.jsx
+++ client/src/components/ratings/ReviewList.jsx
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom';
 import ReviewTile from './ReviewTile.jsx';
 
 const ReviewList = ({ reviews }) => {
+
+  if (reviews.length === 0) {
+    return (
+      <button>Write A Review +</button>
+    );
+  }
   const formatedReviews = reviews.map( (review, idx) => { return <ReviewTile key={ idx } review={ review }/> })
-  console.log(formatedReviews);
-
   const [numReviewsRendered, setNumReviewsRendered] = useState(2);
-
   const addReviews = () => {
     setNumReviewsRendered(prev => prev + 2);
   }
+
   return (
     <>
       <div className="review-list-container">
