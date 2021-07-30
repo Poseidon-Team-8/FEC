b6f1b17e58da7ab91931d0588f9c1a06785bb3f3
Sho Kondo <notshokondo@gmail.com>
2021-07-29 23:54:22 -0700

    feedback scale labels rendering to corresponding characteristc

--- client/src/components/ratings/CharacteristicEntry.jsx
+++ client/src/components/ratings/CharacteristicEntry.jsx
@@ -6,6 +6,13 @@ const CharacteristicEntry = ({ characteristic, value }) => {
   console.log(characteristic);
   console.log(percentage)
 
+  const displayFit = (characteristic === 'Fit');
+  const displayLength = (characteristic === 'Length');
+  const displayComfort = (characteristic === 'Comfort');
+  const displayQuality = (characteristic === 'Quality');
+
+  console.log('displayFit: ', displayFit);
+
   const Container = styled.div`
     margin-top: 0.5em;
     width: 100%;
@@ -34,19 +41,66 @@ const CharacteristicEntry = ({ characteristic, value }) => {
     margin-top: 0.4em;
   `;
 
-  const LabelContainer = styled.div`
-    display: flex;
+  const FitScale = styled.div`
+    display: ${props => props.visibility ? 'flex' : 'none'};
     flex-direction: row;
     justify-content: space-between;
+    margin-left: 1em;
+    margin-right: 1em;
   `;
 
+  const LengthScale = styled.div`
+    display: ${props => props.visibility ? 'flex' : 'none'};
+    flex-direction: row;
+    justify-content: space-between;
+    margin-left: 1em;
+    margin-right: 1em;
+  `;
+
+  const ComfortScale = styled.div`
+    display: ${props => props.visibility ? 'flex' : 'none'};
+    flex-direction: row;
+    justify-content: space-between;
+    margin-left: 1em;
+    margin-right: 1em;
+  `;
+
+  const QualityScale = styled.div`
+    display: ${props => props.visibility ? 'flex' : 'none'};
+    flex-direction: row;
+    justify-content: space-between;
+    margin-left: 1em;
+    margin-right: 1em;
+  `;
   return (
-    <Container>
-      <p style={{ "marginLeft": "1em" }}>{ characteristic }</p>
-      <Track>
-        <Arrow src="./icons/arrow.svg" percentage={ percentage }/>
-      </Track>
-    </Container>
+    <>
+      <Container>
+        <p style={{ "marginLeft": "1em", "marginBottom": "0.2em" }}>{ characteristic }</p>
+        <Track>
+          <Arrow src="./icons/arrow.svg" percentage={ percentage }/>
+        </Track>
+        <FitScale visibility={ displayFit }>
+          <p2>Small</p2>
+          <p2>Perfect</p2>
+          <p2>Large</p2>
+        </FitScale>
+        <LengthScale visibility={ displayLength }>
+          <p2>Short</p2>
+          <p2>Perfect</p2>
+          <p2>Long</p2>
+        </LengthScale>
+        <ComfortScale visibility={ displayComfort }>
+          <p2>Poor</p2>
+          <p2>Average</p2>
+          <p2>Great</p2>
+        </ComfortScale>
+        <QualityScale visibility={ displayQuality }>
+          <p2>Poor</p2>
+          <p2>Average</p2>
+          <p2>Great</p2>
+        </QualityScale>
+      </Container>
+    </>
   );
 }
 
