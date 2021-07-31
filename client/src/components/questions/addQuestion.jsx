168e3aa3f6fc71ed47969915ae5c98d58f3854a6
Sam Schnaible II <sam@Sams-MacBook-Pro.local>
2021-07-31 12:14:12 -0700

    reuse addAnswer code for addQuestion

--- client/src/components/questions/addQuestion.jsx
+++ client/src/components/questions/addQuestion.jsx
@@ -3,8 +3,127 @@ import ReactDOM from 'react-dom';
 
 const AddQuestion = (props) => {
 
-  return (
+  const [productName, setProductName] = useState('');
+  const [answerInput, setAnswerInput] = useState('');
+  const [nameInput, setNameInput] = useState('');
+  const [emailInput, setEmailInput] = useState('');
+  const [isClicked, setIsClicked] = useState(false);
 
+  const validate = () => {
+    let requiredAnswer = '';
+    let requiredName = '';
+    let requiredEmail = '';
+    let emailValid = emailInput.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+    let requiredValidEmail = '';
+    let message = 'You must enter the following:';
+
+    if ( answerInput === '' ) {
+      requiredAnswer = 'An answer';
+    }
+    if ( nameInput === '' ) {
+      requiredName = 'A name';
+    }
+    if ( emailInput === '' ) {
+      requiredEmail = 'An email';
+    }
+    if ( !emailValid ) {
+      requiredValidEmail = 'A valid email';
+    }
+    if ( requiredAnswer || requiredName || requiredEmail || requiredValidEmail) {
+      let invalidInfo = [requiredAnswer, requiredName, requiredEmail, requiredValidEmail];
+      let alertMessage = `${message}`;
+      invalidInfo.forEach(requiredInfo => {
+        if ( requiredInfo) {
+          alertMessage += '\n' + requiredInfo;
+        }
+      })
+      alert(alertMessage);
+      return true;
+    }
+  }
+
+  const getProductInfo = () => {
+    axios({
+      method: 'get',
+      url: '/productInfo',
+      headers: {
+        id: `${productId}`
+      }
+    })
+    .then( results => {
+      setProductName(results.data.name);
+    })
+    .catch(error => {
+      console.log('THIS IS CLIENT SIDE ERROR', error)
+    })
+  }
+
+  const handleOnSubmit = () => {
+    if (validate()) {
+      return;
+    }
+    axios({
+      method: 'post',
+      url: '/addAnswer',
+      data: {
+        body: `${answerInput}`,
+        name: `${nameInput}`,
+        email: `${emailInput}`
+      },
+      headers: {
+        id: `${questionId}`
+      }
+    })
+    .then(result => {
+      setAnswerInput('');
+      setNameInput('');
+      setEmailInput('');
+      setIsClicked(false);
+    })
+    .catch(error => {
+      console.log('CLIENT SIDE ERROR', error)
+    })
+  }
+  useEffect(() => {
+    getProductInfo();
+  }, []);
+
+// toggle button once I get response so I can clear fields firts
+  return (
+    <div>
+      { !isClicked ? <button onClick={() => setIsClicked(true)}>Add Answer</button> :
+      <div className='modal-container' onClick={() => setIsClicked(false)}>
+        <div className='modal-content'>
+          <h2>Submit Your Answer </h2>
+          <h3>{body}: </h3>
+          <p>{productName}</p>
+          <form>
+            <label>
+              Your Answer*:
+              <input className='submit-answer' type='text' maxLength='1000' required
+              value={answerInput} onChange={(e) => setAnswerInput(e.target.value)}/>
+            </label>
+            <label>
+              What is your nickname*:
+              <input className='submit-name' type='text' maxLength='60' required
+              value={nameInput} placeHolder='Example: jack543!'
+              onChange={(e) => setNameInput(e.target.value)}/>
+            </label>
+            <label>
+              Your email*:
+              <input className='submit-email' type='email' maxLength='60' required
+              value={emailInput} placeHolder='Example: jack@email.com'
+              onChange={(e) => setEmailInput(e.target.value)}/>
+            </label>
+            <p>For authentication reasons, you will not be emailed</p>
+              <input className='submit-answerButton' type='button' value='Submit Answer'
+              onClick={() => handleOnSubmit()}
+              />
+          </form>
+        </div>
+      </div>
+    }
+    </div>
   )
 }
 
