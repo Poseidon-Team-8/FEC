9d8ea7dd0d6b4da37bf627e3cfe97e468af06b47
Sho Kondo <notshokondo@gmail.com>
2021-08-01 01:09:44 -0700

    refactored product breakdown

--- client/src/components/ratings/ProductBreakdown.jsx
+++ client/src/components/ratings/ProductBreakdown.jsx
@@ -1,13 +1,51 @@
 import React, { useState, useContext } from 'react';
 import CharacteristicEntry from './CharacteristicEntry.jsx';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  margin-top: 0.5em;
+  width: 100%;
+`;
+
+const Track = styled.div`
+  display: flex;
+  align-content: center;
+  width: auto;
+  height: 10px;
+  background-color: #dee3e3;
+  border-radius: 2px;
+  margin-left: 1em;
+  margin-right: 1em;
+  position: relative;
+  align-items: center;
+`;
+
+const Arrow = styled.img`
+  height: auto;
+  width: auto;
+  position: absolute;
+  top: 0;
+  left: ${props => props.percentage}%;
+  transform: translate(-50%, -50%);
+  margin-top: 0.4em;
+`;
 
 const ProductBreakdown = ({ characteristics }) => {
    return (
     <div>
       {
         Object.keys(characteristics).map( (characteristic, idx) => {
-          return <CharacteristicEntry key={ idx }characteristic={ characteristic } value={ characteristics[characteristic].value } />
-        })
+          const { value } =  characteristics[characteristic];
+          const percentage = (value/5*100).toFixed(2);
+          return (
+          <Container>
+            <p style={{ "marginLeft": "1em", "marginBottom": "0.2em" }}>{ characteristic }</p>
+            <Track>
+              <Arrow src="./icons/arrow.svg" percentage={ percentage }/>
+            </Track>
+            <CharacteristicEntry key={ idx } characteristic={ characteristic } />
+          </Container>
+        )})
       }
     </div>
   )
