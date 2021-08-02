3a6aee57b288002fa6729de51efc21edbc61b6e8
Bruno <bberastain@gmail.com>
2021-08-02 11:25:13 -0700

    styleSelector tests

--- __tests__/overview/styleSelector.test.js
+++ __tests__/overview/styleSelector.test.js
@@ -5,13 +5,22 @@ import mockData from '../../mockData.js';
 
 const {styles, styleIndex} = mockData;
 
-// describe('display somethin', () => {
-//   it('display thing', () => {
-//     const render = new ShallowRenderer();
-//     render.render(<StyleSelector
-//       styles={styles}
-//       styleIndex={styleIndex} />);
-//     const result = render.getRenderOutput()
-//     expect(result.props.children[0].props.children).toBe('Heir Force Ones')
-//   })
-// })
+describe('display style selector', () => {
+  it('display style name', () => {
+    const render = new ShallowRenderer();
+    render.render(<StyleSelector
+      styles={styles}
+      styleIndex={styleIndex} />);
+    const result = render.getRenderOutput()
+    expect(result.props.children[0].props.children[1].props.children).toBe('White & White')
+  })
+
+  it('display correct thumbnail image', () => {
+    const render = new ShallowRenderer();
+    render.render(<StyleSelector
+      styles={styles}
+      styleIndex={styleIndex} />);
+    const result = render.getRenderOutput()
+    expect(result.props.children[1].props.children[0].props.src).toBe('https://images.unsplash.com/photo-1544441892-794166f1e3be?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80')
+  })
+})
